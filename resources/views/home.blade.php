6c3da698395f0aaa9397094cf253ea7c29a34146
InsidiousMiss <crashd.burnd@gmail.com>
2016-05-07 18:57:28 -0400

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<header><h2>Dashboard</h2></header>
+<p>
+    You are logged in!
+</p>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('full_title', 'GOU Course Evaluation')
+
+@section('content')
+<header><h2>Welcome</h2></header>
+<p>
+    Your Application's Landing Page.
+</p>
+@endsection
