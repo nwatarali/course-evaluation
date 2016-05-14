8a3e8919cc65f991b53f16ea45249512ce2ce611
nwatarali <chetanwatarali@gmail.com>
2016-05-14 21:51:52 +0100

    Created a page for Evaluation Process

---
+++ database/migrations/2016_05_11_200303_create_evaluations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvaluationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluations', function (Blueprint $table) {
+                                    $table->increments('id');
+                                    $table->integer('student_id')->unsigned();
+                                    $table->foreign('student_id')->references('id')->on('students');
+                                    $table->integer('lecturer_id')->unsigned();
+                                    $table->foreign('lecturer_id')->references('id')->on('lecturers');
+                                    $table->string('year',9);
+                                    $table->tinyinteger('session');
+                                    $table->string('grade_received', 1)->nullable();
+                                    $table->text('comments')->nullable();
+                                    $table->timestamps();
+        });
+    }
+
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('evaluations');
+    }
+}
